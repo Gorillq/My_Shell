aab6c482a8fb775196bbd30aecc72d95a2e7c03d
Gorillq <kamil.kownacki3@gmail.com>
2025-02-06 14:52:32 +0100

    bugfix

--- lib/shella/addons.rb
+++ lib/shella/addons.rb
@@ -4,15 +4,15 @@ module Addons
       @memo = []
 		end
 		def fax(input)
-			File.open(input, "r") do |file|
-				file.each_line do |line|
-					@memo << line
-				end
+		  File.open(input, "r") do |file|
+			  file.each_line do |line|
+				  @memo << line
+			  end
 			end
 		end
     def putfax
       memo = @memo
-      IO.popen(memo, "w") {|pipe| pipe.puts memo}
+      IO.popen('pbcopy', "w") { |pipe| pipe.puts memo }
     end
 	end
 end
