402437e8d71ca97849f087d672ecdca9eed052f3
Gorillq <kamil.kownacki3@gmail.com>
2025-02-14 16:22:46 +0100

    more autocomplete

--- lib/autocompleter.rb
+++ lib/autocompleter.rb
@@ -3,7 +3,18 @@ require 'readline'
 
 class Autocompleter
   def initialize
-    @command = ["mkdir", "rmdir", "touch", "echo", "exit", "help", "grep", "sort", "uniq", "cat", "cpc", "cvc", "ben"]
+    @command = [
+  "mkdir", "rmdir", "touch", "echo", "exit", "help", "grep", "sort", "uniq", "cat",
+  "chmod", "chown", "move", "less", "tail", "head", "find", "history",
+  "kill", "link", "more", "less", "tail", "head", "cpc", "cvc", "ben",
+  "top", "tar", "gzip", "gunzip", "sed", "awk", "expr", "sleep", "wait",
+  "which", "whereis", "whoami", "groups", "passwd", "sudo",
+  "ssh", "scp", "ping", "wget", "curl", "ftp", "telnet", "date", "cal", "clear",
+  "diff", "patch", "uname", "env", "export", "set", "unset", "source", "alias",
+  "unalias", "ulimit", "jobs", "screen", "tmux", "time", "watch",
+  "xargs", "yes", "zip", "unzip", "mount", "umount", "dd", "mkfs", "fdisk",
+  "shutdown", "reboot", "logout"
+]
   end
 
   def completion_proc(current_dir = '.')
